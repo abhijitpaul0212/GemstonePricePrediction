33eea706c8fbde7375076d7e28af9d9477e0ab79
abhijitpaul0212 <abhijitpaul0212@gmail.com>
2023-10-30 02:05:56 +0530

    Updated utils.py

--- src/GemstonePricePrediction/utils/utils.py
+++ src/GemstonePricePrediction/utils/utils.py
@@ -13,16 +13,17 @@ from src.GemstonePricePrediction.exception import CustomException
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
+        
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
 
     except Exception as e:
+        logging.info('Exception occured while saving object')
         raise CustomException(e, sys)
-   
-   
+
+
 def evaluate_model(X_train, y_train, X_test, y_test, models: dict) -> dict:
     try:
         report = {}
@@ -53,6 +54,7 @@ def load_object(file_path):
     try:
         with open(file_path, 'rb') as file_obj:
             return pickle.load(file_obj)
+        
     except Exception as e:
-        logging.info('Exception Occured in load_object function utils')
+        logging.info('Exception occured while loading object')
         raise CustomException(e, sys)
